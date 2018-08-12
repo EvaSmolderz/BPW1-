1051b805a1b3633bb20604d2a7e7715397f2b8fb
EvaSmolderz <36568809+EvaSmolderz@users.noreply.github.com>
2018-08-13 01:52:44 +0200

    Cookies, Enemy, Colliders, Mainmenu, Exit, Victory
    The game works. NO AUDIO. NO BACKGROUND. enemy assets.

---
+++ MovingScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 MoveVelocity;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    }
+
+
+
+
+
+    private void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.velocity = movement * speed;
+
+    }
+}
