3c9c733ad29d23f840e305c49f07818d764b2563
EvaSmolderz <36568809+EvaSmolderz@users.noreply.github.com>
2018-08-13 04:29:36 +0200

    Scripts

---
+++ EnemyAttack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider col)
+    {
+        HealthBarScript.health -= 10f;
+		
+	}
+}
---
+++ ShootScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShootScript : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform shootPoint;
+    public Transform previewCube;
+    public AudioClip ShotSound;
+    public AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            previewCube.gameObject.SetActive(true);
+            print("HIT HIT");
+            audioSource.PlayOneShot(ShotSound);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(shootPoint.position, shootPoint.forward, out hit, 10f))
+            {
+                previewCube.transform.position = hit.point + hit.normal * 0.5f;
+            }
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            //previewCube.gameObject.SetActive(false);
+            RaycastHit hit;
+            if (Physics.Raycast(shootPoint.position, shootPoint.forward, out hit, 10f))
+            {
+                Instantiate(bullet, previewCube.transform.position, Quaternion.LookRotation(shootPoint.forward));
+            }
+        }
+    }
+}
+
